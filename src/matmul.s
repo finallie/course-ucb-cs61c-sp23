f6a4d7d082f43713ae76fe98229dde4e9d93d425
finallie <1006492957@qq.com>
2023-09-28 23:28:31 +0800

    task 5

--- src/matmul.s
+++ src/matmul.s
@@ -24,40 +24,67 @@
 #     this function terminates the program with exit code 38
 # =======================================================
 matmul:
+    addi sp sp -8
+    sw ra 0(sp)
+    sw s0 4(sp)
 
+    mv s0 a0
     # Error checks
-
-
+    li t0 1
+    li a0 38
+    blt a1 t0 exception
+    blt a2 t0 exception
+    blt a4 t0 exception
+    blt a5 t0 exception
+    bne a2 a4 exception
     # Prologue
-
-
+    mv a0 s0
+    li t0 0 #i=0
 outer_loop_start:
-
-
-
-
+    beq t0 a1 outer_loop_end
+    li t1 0 # j=0
 inner_loop_start:
-
-
-
-
-
-
-
-
-
-
-
-
+    beq t1 a5 inner_loop_end
+    
+    li t2 0 # k=0
+    li t3 0 # sum=0
+loop_k:
+    beq t2 a2 loop_k_end
+    #sum+=m0[i][k]*m1[k][j]
+    mul t4 t0 a2
+    add t4 t4 t2
+    slli t4 t4 2
+    add t4 t4 a0
+    lw t4 0(t4)
+    mul t5 t2 a5
+    add t5 t5 t1
+    slli t5 t5 2
+    add t5 t5 a3
+    lw t5 0(t5)
+    mul t4 t4 t5
+    add t3 t3 t4
+    addi t2 t2 1
+    j loop_k
+loop_k_end:
+#d[i][[j]=sum
+    mul t4 t0 a5
+    add t4 t4 t1
+    slli t4 t4 2
+    add t4 t4 a6
+    sw t3 0(t4)
+    addi t1 t1 1
+    j inner_loop_start
 inner_loop_end:
-
-
-
-
+    addi t0 t0 1
+    j outer_loop_start
+exception:
+    call exit
 outer_loop_end:
 
 
     # Epilogue
-
+    lw ra 0(sp)
+    lw s0 4(sp)
+    addi sp sp 8
 
     jr ra
